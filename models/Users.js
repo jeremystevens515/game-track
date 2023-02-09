abedf27e49c7c048c213949789ca702ec45b0201
Andres-Zetina <zetinaandres700@gmail.com>
2023-02-09 14:42:29 -0500

    added connection.js

---
+++ models/Users.js
@@ -0,0 +1,30 @@
+const {Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection")
+
+class Users extends Model()
+
+
+Users.init(
+{
+    user_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+})
