adb1716503b535dd494888f48507652d5be181d4
Andres-Zetina <zetinaandres700@gmail.com>
2023-02-10 13:48:39 -0500

    added get request to get game data to show in homepage

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const {Games} = require('../models')
+
 
 router.get('/', (req, res) => {
     res.render('../views/homepage')
+})
+//have to make get request to games table 
+router.get('/',async (req, res) =>{
+try {
+    const games = await Games.findAll({
+        attributes: ['name', 'cover', 'genres'],
+    });
+    res.json(games)
+}catch(err){
+    console.error(err);
+    res.status(500).json({message:'Error retriving the games'})
+}
 })
 
 module.exports = router;
