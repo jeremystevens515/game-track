0b40e30ba89b17bd645c842338db362cef9d18b0
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-02-10 15:42:27 -0500

    added create account, login, and logout POST routes to user-routes.js

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -2,13 +2,64 @@ const router = require("express").Router();
 const { Users, Reviews, Wishlist, Games } = require("../models");
 // GET requests--------------------------------------------------
 router.get("/reviews", async (req, res) => {
-	const userReviews = await res.render("../views/user-reviews");
+	const userReviews = await Reviews.findAll({
+		where: {
+			user_id: req.session.user_id,
+		},
+	});
+	res.render("../views/user-reviews");
 });
 
-// router.get("wishlist", (req, res) => {
-// 	res.render("");
-// });
-
 // POST requests--------------------------------------------------
+// create account
+router.post("/create_account", async (req, res) => {
+	try {
+		const userData = await Users.create(req.body);
+		req.session.save(() => {
+			(req.session.user_id = userData.id), (req.session.loggedIn = true);
 
+			res.status(200).json(userData);
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// login
+router.post("/login", async (req, res) => {
+	try {
+		const userData = await Users.findOne({
+			where: {
+				[Op.or]: [{ username: req.body.username }, { email: req.body.email }],
+			},
+		});
+
+		if (!userData) {
+			res.status(400).json({ message: "No user found" });
+			return;
+		}
+
+		const validatePassword = await userData.checkPassword(req.body.password);
+
+		if (!validatePassword) {
+			res.status(400).json({ message: "Incorrect password" });
+		}
+
+		req.session.save(() => {
+			(req.session.user_id = userData.id), (req.session.loggedIn = true);
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.post("/logout", async (req, res) => {
+	if (req.session.loggedIn) {
+		req.session.destroy(() => {
+			res.status(204).end();
+		});
+	} else {
+		res.status(404).end();
+	}
+});
 module.exports = router;
