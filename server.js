cbfffd684d5506a80668fbe5f7d13377a2ed1a99
Andres-Zetina <zetinaandres700@gmail.com>
2023-02-10 11:42:56 -0500

    homepage started

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT||3001;
-
-
-
-const hbs = exphbs.create({})
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-
 app.listen(PORT,()=>{
     console.log("Hello Team!");
 });
