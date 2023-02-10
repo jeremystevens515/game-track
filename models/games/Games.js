bd42370d9250181db72a287771ba07e9637b1ac7
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-02-09 20:32:40 -0500

    added 'games' and 'user'  subdirectories in db directory for organization. added files to /db/games for Genres model, Publishers model, and Platforms model

---
+++ models/games/Games.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const Sequelize = require("../config/connection");
+
+class Games extends Model {}
+
+Games.init({
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+	},
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	publisher: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	release_date: {
+		type: DataTypes.DATE,
+		allowNull: false,
+	},
+	cover_image: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+});
