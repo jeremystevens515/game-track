5b6b50210a48c06e584cdcefe1b1eae75bc1f988
Andres-Zetina <zetinaandres700@gmail.com>
2023-02-12 11:29:45 -0500

    finally got cover image to work inside gamespage

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const {Games} = require('../models')
+const { Users, Reviews, Wishlist, Games } = require("../models");
 
 
 
-//get request to / getting game data like name cover and genre of game 
+// get request to / getting game data like name cover and genre of game 
 router.get('/',async (req, res) =>{
 try {
     const gamesData = await Games.findAll({
-        attributes: ['name', 'cover', 'genres'],
+        attributes: ['name', 'cover', 'genres','id'],
     });
     gamesData.forEach(game => {
         const genresNames = game.genres.map(genre => genre.name)
@@ -22,6 +22,35 @@ try {
     res.status(500).json({message:'Error retriving the games'})
 }
 })
+//get request to take you to a specifics game page based off the games id 
+router.get('/:id', async (req, res) => {
+    try{
+        const gamesInfo = await Games.findOne({
+            where:{id: req.params.id},
+            attributes:['name','cover','genres', "involved_companies",'first_release_date','summary','total_rating','similar_games'],
+        });     
+        let imageId;
+        if (gamesInfo.cover){
+            imageId = gamesInfo.cover.image_id;
+        }
+        res.render('gamepage',{gamesInfo, imageId})
+    }catch (err){
+        console.error(err);
+        res.status(500).json({message:'Error retriving the game info'})
+    }
+})
+
+
+//attempt at fetching the api data instead of the seed data 
+// router.get('/', async (req, res) => {
+//     try {
+//         const response = await fetch('https//');
+//         const data = await response.json();
+//         res.render('homepage', {gameData:data});
+//     }catch (err){
+//         res.status(500).json ({message: 'Error retriveing api game data'})
+//     }
+// })
 
 
 module.exports = router;
