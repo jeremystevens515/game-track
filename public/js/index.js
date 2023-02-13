4d7927e6d9fb47adc05f4ce50261aa8a485554e7
neffej <118024182+neffej@users.noreply.github.com>
2023-02-13 14:29:42 -0500

    added POST and DELETE routes for wishlists on user-routes.js. Updated FE to help grab info for POST body

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
+// Send DELETE request to server to Logout
+async function logOut(event){
+    event.preventDefault();
+    console.log("Click!")
+}
+
+document.querySelector('#logout-btn').addEventListener('click', logOut)
 
