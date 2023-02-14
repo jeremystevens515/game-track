affd3d6e30047e518ae50fa75382ae039736a231
Andres-Zetina <zetinaandres700@gmail.com>
2023-02-14 15:57:10 -0500

    ready to deploygit add -Agit add -A

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+	console.log('hi show me this please',process.env.JAWSDB_URL)
 	sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
 	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
